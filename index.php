4a1dccd7e019ff83e0ae359866f7a80821d23b4f
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-02-16 13:20:18 +0100

    Impostato Esercizio

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+class Movie {
+
+};
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>PHP OOP 1</title>
+</head>
+<body>
+    <header class="container">
+        <h1>
+            PHP OOP 1
+        </h1>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
